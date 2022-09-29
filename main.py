4f12425f56e49c51277815034606f0bcf36b9b0b
Thiago Macedo <thialmacedo@gmail.com>
2022-09-29 14:50:41 -0300

    initialized new ag

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from ag import AlgoritmoGenetico
 
 def main():
     lab = Labirinto('labirinto1.txt')
-    ag = AlgoritmoGenetico(1000, 100, 1, False, 5, 0.008, False)
+    ag = AlgoritmoGenetico(max_geracoes=1000,
+                           pop_size=100,
+                           n_elites=5,
+                           reposition=True,
+                           reposition_n=10,
+                           taxa_mutacao=0.01,
+                           smart_first_gen=True)
     ag.fit(lab)
 
 
