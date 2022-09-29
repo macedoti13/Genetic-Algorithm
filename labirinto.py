084c425e3ede45b7fdfa59aeca62f7f3dae01254
Thiago Macedo <thialmacedo@gmail.com>
2022-09-29 14:18:40 -0300

    docstrings para labirinto

--- labirinto.py
+++ labirinto.py
@@ -3,14 +3,26 @@ import numpy as np
 class Labirinto:
 
     def __init__(self, arquivo: str) -> None:
-        """"""
+        """Inicializa o objeto labirinto com um arquivo que sera utilizado para a 
+           criacao do caminho.
+
+        Args:
+            arquivo (str): Nome do arquivo. 
+        """        
         self.caminho = self._cria_lab_de_arquivo(arquivo)
         self.pos_inicial = self._retorna_pos_inicial()
         self.pos_comidas = self._retorna_pos_comidas()
 
 
     def _cria_lab_de_arquivo(self, arquivo: str) -> np.ndarray:
-        """"""
+        """A partir de um arquivo, gera uma matriz da biblioteca numpy.
+
+        Args:
+            arquivo (str): Nome do arquivo.
+
+        Returns:
+            np.ndarray: Matriz do numpy representando o labirinto. 
+        """        
         with open(arquivo, 'r') as f:
             linhas = f.readlines()
             dim = int(linhas[0])
@@ -27,7 +39,7 @@ class Labirinto:
 
 
     def _retorna_pos_inicial(self) -> tuple:
-        """"""
+        """Retorna a posicao inicial de onde o indivio comeca a exploracao."""
         posicao = np.where(self.caminho == -1)
         pos_inicial = (posicao[0][0], posicao[1][0])
 
@@ -35,7 +47,11 @@ class Labirinto:
 
 
     def _retorna_pos_comidas(self) -> list:
-        """"""
+        """Coloca em uma lista as posicoes onde estao localizadas as comidas.
+
+        Returns:
+            list: Lista com as coordenadas das comidas.
+        """
         posicoes = np.where(self.caminho == 2)
         pos_comidas = []
 
@@ -47,7 +63,11 @@ class Labirinto:
 
     
     def marca_posicoes_percorridas(self, lista_posicoes: list) -> None:
-        """"""
+        """Muda o valor das posicoes do labirinto de acordo com uma lista de posicoes. 
+
+        Args:
+            lista_posicoes (list): Lista de posicoes percorridas por um Individuo.
+        """        
         for posicao in lista_posicoes:
             linha = posicao[0]
             coluna = posicao[1]
@@ -58,7 +78,7 @@ class Labirinto:
 
 
     def print(self) -> None:
-        """"""
+        """Coloca em uma lista varias strings que em conjunto, mostram o labirinto."""
         frases = []
         frases.append('-'*34)
         for linha in self.caminho:
